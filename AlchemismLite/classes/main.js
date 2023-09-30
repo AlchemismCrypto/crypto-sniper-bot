3b9566d3b2d35b37e734700a3aa21f2c224d3ed3
AlchemismTeam <144623498+AlchemismTeam@users.noreply.github.com>
2023-09-30 16:51:44 +0800

    Delete AlchemismLite directory

--- AlchemismLite/classes/main.js
+++
@@ -1,8 +0,0 @@
-const msg = require('./msg.js');
-const config = require('./config.js');
-const cache = require('./cache.js');
-const network = require('./network.js');
-
-require('./init.js');
-
-module.exports = {msg, config, cache, network};
